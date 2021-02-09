56b61231fb1c1424d88521af0c4edfa5859cffe6
grippal <lucagrip@yahoo.com>
2021-02-09 16:48:28 -0500

    Copied base code over from cyber-blog

---
+++ _site/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    syntax: 'postcss-scss',
+    plugins: [
+      // ...
+      require('tailwindcss')('./tailwind.config.js'),
+      require('autoprefixer'),
+      // ...
+    ]
+  }
---
+++ _site/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./_site/**/*.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+      'roboto-mono': ['Roboto\\ mono', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
