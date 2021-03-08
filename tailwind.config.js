a20b4dddff65c6b2dfe4fe9cbd3725989943068d
grippal <lucagrip@yahoo.com>
2021-03-08 15:46:08 -0500

    updated about me page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,38 @@
+const { color } = require('ansi-styles')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./_site/**/*.html'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
       'roboto': ['Roboto', 'sans-serif'],
       'roboto-mono': ['Roboto\\ mono', 'sans-serif']
+    },
+    colors: {
+      blueGray: colors.blueGray,
+      coolGray: colors.coolGray,
+      gray: colors.gray,
+      trueGray: colors.trueGray,
+      warmGray: colors.warmGray,
+      white: colors.white,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightBlue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
     },
     extend: {},
   },
